ae2a526c1aa3028d323e8e1bdbbad5a2f30e8d04
Riko <rikomintea1827@gmail.com>
2021-09-29 01:13:46 +0900

    アニメーション追加

--- js/scroll.js
+++ js/scroll.js
@@ -4,33 +4,168 @@ ScrollReveal().reveal('h3',{
 });
 ScrollReveal().reveal('h4',{
     delay:500,
-    duration: 3000, // アニメーションの完了にかかる時間
-    viewFactor: 0.7, // 0~1,どれくらい見えたら実行するか
+    duration: 2000, 
+    viewFactor: 0.7,
 });
-ScrollReveal().reveal('.worship h5',{
-    delay:500,
-    duration: 3000, // アニメーションの完了にかかる時間
-    viewFactor: 0.7, // 0~1,どれくらい見えたら実行するか
-});
+// ScrollReveal().reveal('h5',{
+//     delay:500,
+//     duration: 2000, 
+//     viewFactor: 0.7, 
+// });
 
+
+
+// 礼拝
 ScrollReveal().reveal('.worship img',{
-    duration: 3000, // アニメーションの完了にかかる時間
-    viewFactor: 0.7, // 0~1,どれくらい見えたら実行するか
+    delay:200,
+    duration: 5000, 
+    viewFactor: 0.7,
 });
-
 ScrollReveal().reveal('#worship_text1',{
-    delay:1500,
-    duration: 3000, // アニメーションの完了にかかる時間
+    delay:200,
+    duration: 4000,
 });
 ScrollReveal().reveal('#worship_text2',{
-    delay:1800,
-    duration: 3000, // アニメーションの完了にかかる時間
+    delay:400,
+    duration: 4000, 
 });
 ScrollReveal().reveal('#worship_text3',{
-    delay:2100,
-    duration: 3000, // アニメーションの完了にかかる時間
+    delay:600,
+    duration: 4000, 
 });
 ScrollReveal().reveal('#worship_text4',{
-    delay:2400,
-    duration: 3000, // アニメーションの完了にかかる時間
+    delay:800,
+    duration: 4000, 
 });
+
+
+
+
+// 校風
+ScrollReveal().reveal('.schoolspirit_content img',{
+    delay:200,
+    duration: 5000, 
+    viewFactor: 0.7,
+});
+
+
+ScrollReveal().reveal('#schoolspirit_text1',{
+    delay:200,
+    duration: 4000,
+});
+ScrollReveal().reveal('#schoolspirit_text2',{
+    delay:400,
+    duration: 4000, 
+});   
+ScrollReveal().reveal('#schoolspirit_text3',{
+    delay:600,
+    duration: 4000,
+});
+ScrollReveal().reveal('#schoolspirit_text4',{
+    delay:800,
+    duration: 4000, 
+});
+ScrollReveal().reveal('#schoolspirit_text5',{
+    delay:1000,
+    duration: 4000, 
+});
+
+
+
+
+
+// 授業
+ScrollReveal().reveal('.education img',{
+    duration: 2000, 
+    viewFactor: 0.7,
+});
+ScrollReveal().reveal('.education_title p',{
+    delay:500,
+    duration: 2000, 
+    viewFactor: 0.7, 
+});
+
+
+
+
+
+ScrollReveal().reveal('#education_content1 h6',{
+    delay:200,
+    duration: 3000, 
+});
+ScrollReveal().reveal('#education_content1 p',{
+    delay:400,
+    duration: 3000,
+});
+
+
+
+ScrollReveal().reveal('#education_content2 h6',{
+    delay:300,
+    duration: 3000,
+});
+ScrollReveal().reveal('#education_content2 p',{
+    delay:500,
+    duration: 3000,
+});
+
+
+
+ScrollReveal().reveal('#education_content3 h6',{
+    delay:400,
+    duration: 3000, 
+});
+ScrollReveal().reveal('#education_content3 p',{
+    delay:600,
+    duration: 3000,
+});
+
+
+
+
+// キャンパス
+ScrollReveal().reveal('.campus_img',{
+    delay:200,
+    duration: 5000, 
+    viewFactor: 0.7,
+});
+
+ScrollReveal().reveal('#campus_text1',{
+    delay:400,
+    duration: 2000, 
+});
+ScrollReveal().reveal('#campus_text2',{
+    delay:600,
+    duration: 2000, 
+});
+ScrollReveal().reveal('#campus_text3',{
+    delay:800,
+    duration: 2000,
+});
+ScrollReveal().reveal('#campus_text4',{
+    delay:1000,
+    duration: 2000, 
+});
+ScrollReveal().reveal('#campus_text5',{
+    delay:1200,
+    duration: 2000, 
+});
+
+
+
+ScrollReveal().reveal('#campus_content1',{
+    delay:200,
+    duration: 3000, 
+    viewFactor: 0.7,
+});
+ScrollReveal().reveal('#campus_content2',{
+    delay:200,
+    duration: 3000,
+    viewFactor: 0.7,
+});
+ScrollReveal().reveal('#campus_content3',{
+    delay:200,
+    duration: 3000, 
+    viewFactor: 0.7,
+});
+
